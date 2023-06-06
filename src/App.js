33eece0c02e43f987d4529cf94ee4c1fda799082
shubham-sapkal <sapkal.shubham2002@gmail.com>
2023-06-06 22:41:26 +0530

    Big component divied into multiple

--- src/App.js
+++ src/App.js
@@ -1,58 +1,42 @@
 
 // Custom Components
-import ExpenseItem from "./components/ExpenseItem";
+// import ExpenseItem from "./components/ExpenseItem";
+
+import Expenses from "./components/Expenses";
 
 function App(){
 
     const expenses = [
         {
-          id: 'e1',
-          title: 'Toilet Paper',
+          id: "e1",
+          title: "Toilet Paper",
           amount: 94.12,
           date: new Date(2020, 7, 14),
         },
-        { 
-            id: 'e2', 
-            title: 'New TV', 
-            amount: 1799.49, 
-            date: new Date(2021, 2, 12) 
+        {
+          id: "e2",
+          title: "New TV",
+          amount: 1799.49,
+          date: new Date(2021, 2, 12),
         },
         {
-          id: 'e3',
-          title: 'Car Insurance',
+          id: "e3",
+          title: "Car Insurance",
           amount: 294.67,
           date: new Date(2021, 2, 28),
         },
         {
-          id: 'e4',
-          title: 'New Desk (Wooden)',
+          id: "e4",
+          title: "New Desk (Wooden)",
           amount: 450,
           date: new Date(2021, 5, 12),
         },
       ];
 
-
     return (
         <div>
             <h2>This is the first Project ... </h2>
-            <ExpenseItem 
-               title={expenses[0].title} 
-               amount={expenses[0].amount}
-               date={expenses[0].date}/>
-            <ExpenseItem 
-               title={expenses[1].title} 
-               amount={expenses[1].amount}
-               date={expenses[1].date}/>
-            <ExpenseItem 
-               title={expenses[2].title} 
-               amount={expenses[2].amount}
-               date={expenses[2].date}/>
-            <ExpenseItem 
-               title={expenses[3].title} 
-               amount={expenses[3].amount}
-               date={expenses[3].date}/>
-        
-            
+            <Expenses items={expenses} />
         </div>
     )
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,17 @@
-
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-
   // const expenseDate = props.date;
   // const expenseTitle = props.title;
   // const expenseAmount = props.amount;
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
+
       <div className="expense-item__description">
-        <h2> {props.title}  </h2>
+        <h2> {props.title} </h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
