e0435f08568af8f2f1a2c03ad918b0e39a3c7772
shubham-sapkal <sapkal.shubham2002@gmail.com>
2023-06-06 23:09:45 +0530

    added card

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,20 +1,22 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
+import Card from "./Card"; 
+
 function ExpenseItem(props) {
   // const expenseDate = props.date;
   // const expenseTitle = props.title;
   // const expenseAmount = props.amount;
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
         <h2> {props.title} </h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,13 +1,13 @@
 import ExpenseItem from "./ExpenseItem";
 
 import "./Expenses.css";
-
+import Card from "./Card";
 
 function Expenses(props) {
   
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -28,7 +28,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
